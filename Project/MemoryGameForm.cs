719cbf7bf2b78bf5d5c8dad53c875fd3f6a5f48d
Cuchi <42467470+Cu-chi@users.noreply.github.com>
2021-05-30 21:29:16 +0200

    Base game is now working

--- Project/MemoryGameForm.cs
+++ Project/MemoryGameForm.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,31 +13,69 @@ namespace MemoryGame
 {
     public partial class MemoryGameForm : Form
     {
-        private List<Panel> gamePads = new List<Panel>(144); // 144 correspond to the total of pads necessary for an array of 12x12 
+        private List<Panel> gamePads;
+        private bool[] selectedPads;
+        private bool showingPads = false;
+        Dictionary<int, Action<object, EventArgs>> padsFunctions = new Dictionary<int, Action<object, EventArgs>>();
 
         public MemoryGameForm()
         {
             InitializeComponent();
+            padsFunctions.Add(1, PadsSelect1);
+            padsFunctions.Add(2, PadsSelect2);
+            padsFunctions.Add(3, PadsSelect3);
+            padsFunctions.Add(4, PadsSelect4);
+            padsFunctions.Add(5, PadsSelect5);
+            padsFunctions.Add(6, PadsSelect6);
+            padsFunctions.Add(7, PadsSelect7);
+            padsFunctions.Add(8, PadsSelect8);
+            padsFunctions.Add(9, PadsSelect9);
         }
 
         private void startButton_Click(object sender, EventArgs e)
         {
-            for (int i = 0; i < 9; i++)
+            Game(9);
+        }
+
+        private async void Game(int padNecessary)
+        {
+            startButton.Visible = false;
+            confirmButton.Visible = true;
+            showingPads = true;
+
+            gamePads = new List<Panel>(padNecessary);
+            selectedPads = new bool[padNecessary];
+
+            Random random = new Random();
+            int genValue = random.Next(padNecessary);
+            for (int i = 0; i < padNecessary; i++)
             {
                 Panel GamePad = new Panel();
-                GamePad.BackColor = Color.FromArgb(241, 250, 238);
+                if (i == genValue) GamePad.BackColor = Color.FromArgb(24, 24, 24);
+                else GamePad.BackColor = Color.FromArgb(241, 250, 238);
                 GamePad.BorderStyle = BorderStyle.FixedSingle;
-                GamePad.Size = PadSizeGen(9);
-                GamePad.Location = PadLocationGen(i + 1, 9, GamePad.Size);
+                GamePad.Size = PadSizeGen(padNecessary);
+                GamePad.Location = PadLocationGen(i + 1, padNecessary, GamePad.Size);
                 Controls.Add(GamePad);
                 GamePad.BringToFront();
 
                 gamePads.Add(GamePad);
             }
 
-            #region First line
-            
-            #endregion
+            await Task.Delay(500);
+
+            for (int i = 0; i < padNecessary; i++)
+            {
+                gamePads[i].BackColor = Color.FromArgb(241, 250, 238);
+                gamePads[i].Click += new EventHandler(padsFunctions[i + 1]);
+            }
+
+            showingPads = false;
+
+            await Task.Delay(1000);
+
+            if (selectedPads[genValue]) MessageBox.Show("BON");
+            else MessageBox.Show("MAUVAIS");
         }
 
         private Point PadLocationGen(int padNumber, int totalPadInThisGame, Size padSize)
@@ -45,7 +84,6 @@ namespace MemoryGame
 
             switch (padNumber)
             {
-                #region First line
                 case 1:
                 if (totalPadInThisGame == 9)
                 {
@@ -67,9 +105,7 @@ namespace MemoryGame
                     posY = gamePanel.Location.Y;
                 }
                 break;
-                #endregion
 
-                #region Second line 
                 case 4:
                 if (totalPadInThisGame == 9)
                 {
@@ -91,9 +127,7 @@ namespace MemoryGame
                     posY = gamePanel.Location.Y + padSize.Height;
                 }
                 break;
-                #endregion
 
-                #region Third line 
                 case 7:
                 if (totalPadInThisGame == 9)
                 {
@@ -115,7 +149,6 @@ namespace MemoryGame
                     posY = gamePanel.Location.Y + padSize.Height * 2;
                 }
                 break;
-                #endregion
             }
 
             return new Point(posX, posY);
@@ -123,10 +156,173 @@ namespace MemoryGame
 
         private Size PadSizeGen(int totalPadInThisGame)
         {
-            int gWidth = gamePanel.Size.Width / (totalPadInThisGame / 3);
-            int gHeight = gamePanel.Size.Height / (totalPadInThisGame / 3);
+            int gWidth = 0, gHeight = 0;
+
+            if (totalPadInThisGame == 9)
+            {
+                gWidth = gamePanel.Size.Width / (totalPadInThisGame / 3);
+                gHeight = gamePanel.Size.Height / (totalPadInThisGame / 3);
+            }
 
             return new Size(gWidth, gHeight);
+        }
+
+        private void confirmButton_Click(object sender, EventArgs e)
+        {
+            Game(9);
+        }
+
+        private void PadsSelect1(object sender, EventArgs e)
+        {
+            if (showingPads) return;
+
+            for (int i = 0; i < selectedPads.Length; i++)
+            {
+                if (selectedPads[i] && i != 0)
+                {
+                    selectedPads[i] = false;
+                    gamePads[i].BackColor = Color.FromArgb(241, 250, 238);
+                }
+            }
+
+            gamePads[0].BackColor = Color.FromArgb(207, 207, 207);
+            selectedPads[0] = true;
+        }
+
+        private void PadsSelect2(object sender, EventArgs e)
+        {
+            if (showingPads) return;
+
+            for (int i = 0; i < selectedPads.Length; i++)
+            {
+                if (selectedPads[i] && i != 1)
+                {
+                    selectedPads[i] = false;
+                    gamePads[i].BackColor = Color.FromArgb(241, 250, 238);
+                }
+            }
+
+            gamePads[1].BackColor = Color.FromArgb(207, 207, 207);
+            selectedPads[1] = true;
+        }
+
+        private void PadsSelect3(object sender, EventArgs e)
+        {
+            if (showingPads) return;
+
+            for (int i = 0; i < selectedPads.Length; i++)
+            {
+                if (selectedPads[i] && i != 2)
+                {
+                    selectedPads[i] = false;
+                    gamePads[i].BackColor = Color.FromArgb(241, 250, 238);
+                }
+            }
+
+            gamePads[2].BackColor = Color.FromArgb(207, 207, 207);
+            selectedPads[2] = true;
+        }
+
+        private void PadsSelect4(object sender, EventArgs e)
+        {
+            if (showingPads) return;
+
+            for (int i = 0; i < selectedPads.Length; i++)
+            {
+                if (selectedPads[i] && i != 3)
+                {
+                    selectedPads[i] = false;
+                    gamePads[i].BackColor = Color.FromArgb(241, 250, 238);
+                }
+            }
+
+            gamePads[3].BackColor = Color.FromArgb(207, 207, 207);
+            selectedPads[3] = true;
+        }
+
+        private void PadsSelect5(object sender, EventArgs e)
+        {
+            if (showingPads) return;
+
+            for (int i = 0; i < selectedPads.Length; i++)
+            {
+                if (selectedPads[i] && i != 4)
+                {
+                    selectedPads[i] = false;
+                    gamePads[i].BackColor = Color.FromArgb(241, 250, 238);
+                }
+            }
+
+            gamePads[4].BackColor = Color.FromArgb(207, 207, 207);
+            selectedPads[4] = true;
+        }
+
+        private void PadsSelect6(object sender, EventArgs e)
+        {
+            if (showingPads) return;
+
+            for (int i = 0; i < selectedPads.Length; i++)
+            {
+                if (selectedPads[i] && i != 5)
+                {
+                    selectedPads[i] = false;
+                    gamePads[i].BackColor = Color.FromArgb(241, 250, 238);
+                }
+            }
+
+            gamePads[5].BackColor = Color.FromArgb(207, 207, 207);
+            selectedPads[5] = true;
+        }
+
+        private void PadsSelect7(object sender, EventArgs e)
+        {
+            if (showingPads) return;
+
+            for (int i = 0; i < selectedPads.Length; i++)
+            {
+                if (selectedPads[i] && i != 6)
+                {
+                    selectedPads[i] = false;
+                    gamePads[i].BackColor = Color.FromArgb(241, 250, 238);
+                }
+            }
+
+            gamePads[6].BackColor = Color.FromArgb(207, 207, 207);
+            selectedPads[6] = true;
+        }
+
+        private void PadsSelect8(object sender, EventArgs e)
+        {
+            if (showingPads) return;
+
+            for (int i = 0; i < selectedPads.Length; i++)
+            {
+                if (selectedPads[i] && i != 7)
+                {
+                    selectedPads[i] = false;
+                    gamePads[i].BackColor = Color.FromArgb(241, 250, 238);
+                }
+            }
+
+            gamePads[7].BackColor = Color.FromArgb(207, 207, 207);
+            selectedPads[7] = true;
+        }
+
+        private void PadsSelect9(object sender, EventArgs e)
+        {
+            if (showingPads) return;
+
+            for (int i = 0; i < selectedPads.Length; i++)
+            {
+                if (selectedPads[i] && i != 8)
+                {
+                    selectedPads[i] = false;
+                    gamePads[i].BackColor = Color.FromArgb(241, 250, 238);
+                }
+            }
+
+            gamePads[8].BackColor = Color.FromArgb(207, 207, 207);
+            selectedPads[8] = true;
         }
     }
 }
