31ecc774199850b3c7a4ff65ab0db62e32d1d09f
Cuchi <42467470+Cu-chi@users.noreply.github.com>
2021-05-30 19:02:49 +0200

    Fonctions to generate size and locations work

--- Project/MemoryGameForm.Designer.cs
+++ Project/MemoryGameForm.Designer.cs
@@ -29,54 +29,57 @@ namespace MemoryGame
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.welcomeLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.gamePanel = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
-            // label1
+            // welcomeLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(250)))), ((int)(((byte)(238)))));
-            this.label1.Location = new System.Drawing.Point(50, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(387, 40);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Welcome to Memory Game!";
+            this.welcomeLabel.AutoSize = true;
+            this.welcomeLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.welcomeLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(250)))), ((int)(((byte)(238)))));
+            this.welcomeLabel.Location = new System.Drawing.Point(50, 9);
+            this.welcomeLabel.Name = "welcomeLabel";
+            this.welcomeLabel.Size = new System.Drawing.Size(387, 40);
+            this.welcomeLabel.TabIndex = 0;
+            this.welcomeLabel.Text = "Welcome to Memory Game!";
             // 
-            // button1
+            // startButton
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(123)))), ((int)(((byte)(157)))));
-            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(250)))), ((int)(((byte)(238)))));
-            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(53)))), ((int)(((byte)(87)))));
-            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(218)))), ((int)(((byte)(220)))));
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(250)))), ((int)(((byte)(238)))));
-            this.button1.Location = new System.Drawing.Point(50, 52);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(387, 76);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "START";
-            this.button1.UseVisualStyleBackColor = false;
+            this.startButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(123)))), ((int)(((byte)(157)))));
+            this.startButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(250)))), ((int)(((byte)(238)))));
+            this.startButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(53)))), ((int)(((byte)(87)))));
+            this.startButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(218)))), ((int)(((byte)(220)))));
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.startButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(250)))), ((int)(((byte)(238)))));
+            this.startButton.Location = new System.Drawing.Point(50, 52);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(387, 76);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "START";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
             // 
-            // panel1
+            // gamePanel
             // 
-            this.panel1.Location = new System.Drawing.Point(12, 134);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(460, 315);
-            this.panel1.TabIndex = 2;
+            this.gamePanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(57)))), ((int)(((byte)(70)))));
+            this.gamePanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.gamePanel.Location = new System.Drawing.Point(62, 134);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.Size = new System.Drawing.Size(360, 360);
+            this.gamePanel.TabIndex = 2;
             // 
             // MemoryGameForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(57)))), ((int)(((byte)(70)))));
-            this.ClientSize = new System.Drawing.Size(484, 461);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(484, 532);
+            this.Controls.Add(this.gamePanel);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.welcomeLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "MemoryGameForm";
@@ -88,9 +91,9 @@ namespace MemoryGame
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label welcomeLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Panel gamePanel;
     }
 }
 
--- Project/MemoryGameForm.cs
+++ Project/MemoryGameForm.cs
@@ -16,5 +16,176 @@ namespace MemoryGame
         {
             InitializeComponent();
         }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            #region First line
+            Panel GamePad1 = new Panel();
+            GamePad1.BackColor = Color.FromArgb(241, 250, 238);
+            GamePad1.BorderStyle = BorderStyle.FixedSingle;
+            GamePad1.Size = PadSizeGen(9);
+            GamePad1.Location = PadLocationGen(1, 9, GamePad1.Size);
+            Controls.Add(GamePad1);
+            GamePad1.BringToFront();
+
+            Panel GamePad2 = new Panel();
+            GamePad2.BackColor = Color.FromArgb(241, 250, 238);
+            GamePad2.BorderStyle = BorderStyle.FixedSingle;
+            GamePad2.Size = PadSizeGen(9);
+            GamePad2.Location = PadLocationGen(2, 9, GamePad2.Size);
+            Controls.Add(GamePad2);
+            GamePad2.BringToFront();
+
+            Panel GamePad3 = new Panel();
+            GamePad3.BackColor = Color.FromArgb(241, 250, 238);
+            GamePad3.BorderStyle = BorderStyle.FixedSingle;
+            GamePad3.Size = PadSizeGen(9);
+            GamePad3.Location = PadLocationGen(3, 9, GamePad3.Size);
+            Controls.Add(GamePad3);
+            GamePad3.BringToFront();
+            #endregion
+
+            #region Second line
+            Panel GamePad4 = new Panel();
+            GamePad4.BackColor = Color.FromArgb(241, 250, 238);
+            GamePad4.BorderStyle = BorderStyle.FixedSingle;
+            GamePad4.Size = PadSizeGen(9);
+            GamePad4.Location = PadLocationGen(4, 9, GamePad4.Size);
+            Controls.Add(GamePad4);
+            GamePad4.BringToFront();
+
+            Panel GamePad5 = new Panel();
+            GamePad5.BackColor = Color.FromArgb(241, 250, 238);
+            GamePad5.BorderStyle = BorderStyle.FixedSingle;
+            GamePad5.Size = PadSizeGen(9);
+            GamePad5.Location = PadLocationGen(5, 9, GamePad5.Size);
+            Controls.Add(GamePad5);
+            GamePad5.BringToFront();
+
+            Panel GamePad6 = new Panel();
+            GamePad6.BackColor = Color.FromArgb(241, 250, 238);
+            GamePad6.BorderStyle = BorderStyle.FixedSingle;
+            GamePad6.Size = PadSizeGen(9);
+            GamePad6.Location = PadLocationGen(6, 9, GamePad6.Size);
+            Controls.Add(GamePad6);
+            GamePad6.BringToFront();
+            #endregion
+
+            #region Third line
+            Panel GamePad7 = new Panel();
+            GamePad7.BackColor = Color.FromArgb(241, 250, 238);
+            GamePad7.BorderStyle = BorderStyle.FixedSingle;
+            GamePad7.Size = PadSizeGen(9);
+            GamePad7.Location = PadLocationGen(7, 9, GamePad7.Size);
+            Controls.Add(GamePad7);
+            GamePad7.BringToFront();
+
+            Panel GamePad8 = new Panel();
+            GamePad8.BackColor = Color.FromArgb(241, 250, 238);
+            GamePad8.BorderStyle = BorderStyle.FixedSingle;
+            GamePad8.Size = PadSizeGen(9);
+            GamePad8.Location = PadLocationGen(8, 9, GamePad8.Size);
+            Controls.Add(GamePad8);
+            GamePad8.BringToFront();
+
+            Panel GamePad9 = new Panel();
+            GamePad9.BackColor = Color.FromArgb(241, 250, 238);
+            GamePad9.BorderStyle = BorderStyle.FixedSingle;
+            GamePad9.Size = PadSizeGen(9);
+            GamePad9.Location = PadLocationGen(9, 9, GamePad9.Size);
+            Controls.Add(GamePad9);
+            GamePad9.BringToFront();
+            #endregion
+        }
+
+        private Point PadLocationGen(int padNumber, int totalPadInThisGame, Size padSize)
+        {
+            int posX = 0, posY = 0;
+
+            switch (padNumber)
+            {
+                #region First line
+                case 1:
+                if (totalPadInThisGame == 9)
+                {
+                    posX = gamePanel.Location.X;
+                    posY = gamePanel.Location.Y;
+                }
+                break;
+                case 2:
+                if (totalPadInThisGame == 9)
+                {
+                    posX = gamePanel.Location.X + padSize.Width;
+                    posY = gamePanel.Location.Y;
+                }
+                break;
+                case 3:
+                if (totalPadInThisGame == 9)
+                {
+                    posX = gamePanel.Location.X + padSize.Width * 2;
+                    posY = gamePanel.Location.Y;
+                }
+                break;
+                #endregion
+
+                #region Second line 
+                case 4:
+                if (totalPadInThisGame == 9)
+                {
+                    posX = gamePanel.Location.X;
+                    posY = gamePanel.Location.Y + padSize.Height;
+                }
+                break;
+                case 5:
+                if (totalPadInThisGame == 9)
+                {
+                    posX = gamePanel.Location.X + padSize.Width;
+                    posY = gamePanel.Location.Y + padSize.Height;
+                }
+                break;
+                case 6:
+                if (totalPadInThisGame == 9)
+                {
+                    posX = gamePanel.Location.X + padSize.Width * 2;
+                    posY = gamePanel.Location.Y + padSize.Height;
+                }
+                break;
+                #endregion
+
+                #region Third line 
+                case 7:
+                if (totalPadInThisGame == 9)
+                {
+                    posX = gamePanel.Location.X;
+                    posY = gamePanel.Location.Y + padSize.Height * 2;
+                }
+                break;
+                case 8:
+                if (totalPadInThisGame == 9)
+                {
+                    posX = gamePanel.Location.X + padSize.Width;
+                    posY = gamePanel.Location.Y + padSize.Height * 2;
+                }
+                break;
+                case 9:
+                if (totalPadInThisGame == 9)
+                {
+                    posX = gamePanel.Location.X + padSize.Width * 2;
+                    posY = gamePanel.Location.Y + padSize.Height * 2;
+                }
+                break;
+                #endregion
+            }
+
+            return new Point(posX, posY);
+        }
+
+        private Size PadSizeGen(int totalPadInThisGame)
+        {
+            int gWidth = gamePanel.Size.Width / (totalPadInThisGame / 3);
+            int gHeight = gamePanel.Size.Height / (totalPadInThisGame / 3);
+
+            return new Size(gWidth, gHeight);
+        }
     }
 }
